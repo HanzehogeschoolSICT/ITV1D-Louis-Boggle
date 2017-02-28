5de8614647f2ee85fd1fe34d575f0793e2cc2590
Louis Matthijssen <louis@u5r.nl>
2017-03-01 00:21:22 +0100

    Sync the legend colors' size with their labels

--- src/displays/LegendDisplay.java
+++ src/displays/LegendDisplay.java
@@ -4,13 +4,22 @@ import data.Settings;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import models.LetterColorModel;
 
 public class LegendDisplay {
     @FXML
+    private Label noMatchColorLabel;
+    @FXML
     private Canvas noMatchColorCanvas;
+
+    @FXML
+    private Label matchColorLabel;
     @FXML
     private Canvas matchColorCanvas;
+
+    @FXML
+    private Label matchStartColorLabel;
     @FXML
     private Canvas matchStartColorCanvas;
 
@@ -19,9 +28,22 @@ public class LegendDisplay {
      */
     @FXML
     public void initialize() {
-        drawLegendColor(noMatchColorCanvas, Settings.LETTER_COLOR);
-        drawLegendColor(matchColorCanvas, Settings.LETTER_MATCH_COLOR);
-        drawLegendColor(matchStartColorCanvas, Settings.LETTER_START_COLOR);
+        syncColorWithLabel(noMatchColorLabel, noMatchColorCanvas, Settings.LETTER_COLOR);
+        syncColorWithLabel(matchColorLabel, matchColorCanvas, Settings.LETTER_MATCH_COLOR);
+        syncColorWithLabel(matchStartColorLabel, matchStartColorCanvas, Settings.LETTER_START_COLOR);
+    }
+
+    /**
+     * Sync the size of the label's associated color canvas.
+     *
+     * @param label  Label to sync.
+     * @param canvas Color canvas to sync.
+     * @param color  Color to draw on the canvas.
+     */
+    private void syncColorWithLabel(Label label, Canvas canvas, LetterColorModel color) {
+        // The colors are square, so height is the same value as width.
+        label.heightProperty().addListener((observable, oldValue, newValue) ->
+                drawLegendColor(canvas, color, newValue.doubleValue()));
     }
 
     /**
@@ -29,17 +51,18 @@ public class LegendDisplay {
      *
      * @param canvas Canvas to draw the legend color on.
      * @param color  Color to draw on the canvas.
+     * @param size   Size of the legend color.
      */
-    private void drawLegendColor(Canvas canvas, LetterColorModel color) {
+    private void drawLegendColor(Canvas canvas, LetterColorModel color, double size) {
+        canvas.setWidth(size);
+        canvas.setHeight(size);
+
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 
         graphicsContext.setFill(color.backgroundColor);
         graphicsContext.setStroke(color.backgroundColor.darker());
 
-        double width = canvas.getWidth();
-        double height = canvas.getHeight();
-
-        graphicsContext.fillRect(0, 0, width, height);
-        graphicsContext.strokeRect(0, 0, width, height);
+        graphicsContext.fillRect(0, 0, size, size);
+        graphicsContext.strokeRect(0, 0, size, size);
     }
 }
